7cc0d02a9b88c4b3440806f1f5617b0555a9d549
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2026-04-28 11:54:47 -0500

    disable syncFromDbgap

--- .github/workflows/ci_override_config_and_start_minio.sh
+++ .github/workflows/ci_override_config_and_start_minio.sh
@@ -38,6 +38,7 @@ fence:
     #    usersync: false  # use the useryaml job instead of the usersync cronjob
     #    userYamlS3Path: s3://cdis-gen3-users/ci/user.yaml
         userYamlS3Endpoint: ${MINIO_SERVICE_URL}
+        syncFromDbgap: false
     FENCE_CONFIG:  # use private config because it takes precedence over public config
         BASE_URL: 'http://fence-service.${NAMESPACE}.svc.cluster.local'
         OPENID_CONNECT:
